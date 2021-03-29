c8bd4113516a758acdc25349dbfc99aeac1ef1cb
Will Abson <will.abson@gmail.com>
2021-03-29 09:53:49 +0100

    Add .travis.yml (#6)
    * Add .travis.yml

---
+++ travis/release.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+echo "=========================== Starting Release Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
+
+
+if [ -z "${RELEASE_VERSION}" ] || [ -z "${DEVELOPMENT_VERSION}" ]; then
+  echo "Please provide a Release and Development version in the format <alfresco-super-pom>-<additional-info> (1.3 or 1.4-SNAPSHOT)"
+  exit 1
+fi
+
+# Use full history for release
+git checkout -B "${TRAVIS_BRANCH}"
+# Add email to link commits to user
+git config user.email "${GIT_EMAIL}"
+
+
+mvn -B release:prepare \
+  -DreleaseVersion="${RELEASE_VERSION}" \
+  -DdevelopmentVersion="${DEVELOPMENT_VERSION}" \
+  -Darguments=-DskipTests \
+  -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
+  -Dusername="${GIT_USERNAME}" \
+  -Dpassword="${GIT_PASSWORD}"
+
+
+mvn deploy -PmavenCentral -Dgpg.passphrase=${GPG_PASSPHRASE}
+
+mvn -B release:perform \
+  -DreleaseVersion="${RELEASE_VERSION}" \
+  -DdevelopmentVersion="${DEVELOPMENT_VERSION}" \
+  -Darguments=-DskipTests \
+  -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
+  -Dusername="${GIT_USERNAME}" \
+  -Dpassword="${GIT_PASSWORD}"
+
+
+popd
+set +vex
+echo "=========================== Finishing Release Script =========================="
+
