b5af583544c3f4b2ec39c06554ef25a45e3a8886
pzurek <Piotr.Zurek@hyland.com>
2023-05-17 15:13:20 +0200

    [release]

--- ci/release.sh
+++ ci/release.sh
@@ -2,14 +2,14 @@
 echo "=========================== Starting Release Script ==========================="
 PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
-pushd "$(dirname "${BASH_SOURCE[0]}")/../../"
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 
 if [ -z "${RELEASE_VERSION}" ] || [ -z "${DEVELOPMENT_VERSION}" ]; then
   echo "Please provide a Release and Development version in the format <alfresco-super-pom>-<additional-info> (1.3 or 1.4-SNAPSHOT)"
   exit 1
 fi
-ls -alh
+
 # Use full history for release
 git checkout -B "${BRANCH_NAME}"
 
